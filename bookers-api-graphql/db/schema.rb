fc47343c318419bff430ce5ae08d6afb7d745714
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-08-12 18:58:36 +0900

    add: user model

--- bookers-api-graphql/db/schema.rb
+++ bookers-api-graphql/db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_12_095528) do
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
