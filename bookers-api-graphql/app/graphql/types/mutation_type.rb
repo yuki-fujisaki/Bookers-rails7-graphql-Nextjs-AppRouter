01458a79729bfa0311005db926a410928cd31515
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-08-14 21:13:53 +0900

    add:  update_userミューテーション作成

--- bookers-api-graphql/app/graphql/types/mutation_type.rb
+++ bookers-api-graphql/app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_user, resolver: Mutations::Users::Create
+    field :update_user, resolver: Mutations::Users::Update
   end
 end
